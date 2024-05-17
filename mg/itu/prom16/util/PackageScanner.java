21f0c692dd214bf45876b1fb1c4357af8d27f123
Andox-web <andox.razaf@gmail.com>
2024-05-17 12:23:13 +0300

    sprint2_2624

--- mg/itu/prom16/util/PackageScanner.java
+++ mg/itu/prom16/util/PackageScanner.java
@@ -6,14 +6,21 @@ import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
 import java.lang.annotation.Annotation;
+import java.util.Map;
+import java.util.HashMap;
 
+import java.lang.reflect.Method;
+
+import mg.itu.prom16.mapping.Mapping;
+import mg.itu.prom16.annotation.Get;
 public class PackageScanner {
-    public static  List<Class<?>> getClassesFromPackage(String packageName,Class<? extends Annotation> annotationClass) throws ClassNotFoundException, IOException {
+
+    public static Map<String,Mapping> getMapping(String packageName,Class<? extends Annotation> annotationClass) throws ClassNotFoundException, IOException {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         String packagePath = packageName.replace(".", "/");
         Enumeration<URL> resources = classLoader.getResources(packagePath);
     
-        List<Class<?>> classes = new ArrayList<>();
+        Map<String,Mapping> map=new HashMap<String,Mapping>();
         while (resources.hasMoreElements()) {
             URL resource = resources.nextElement();
             if (resource.getProtocol().equals("file")) {
@@ -23,13 +30,25 @@ public class PackageScanner {
                         String className = packageName + "." + file.getName().substring(0, file.getName().length() - 6);
                         Class<?> class1=Class.forName(className);
                         if (class1.isAnnotationPresent(annotationClass)) {
-                            classes.add(class1);  
+                            map.putAll(getMethods(class1));                            
                         }
                     }
                 }
             }
         }
-    
-        return classes;
+        return map;
+    }    
+    private static Map<String,Mapping> getMethods(Class<?> clazz) {
+        
+        Map<String,Mapping> methods = new HashMap<String,Mapping>();
+        for (Method method : clazz.getDeclaredMethods()) {
+            Get an= method.getAnnotation(Get.class);
+
+            if(an!=null&& !an.url().isBlank()){
+                methods.put(an.url().trim(),new Mapping(clazz,method));
+            }
+        }
+        return methods;
     }
+    
 }
